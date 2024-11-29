edeb824131b82eb639f2872f55932d3e7aec3cf2
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-11-29 15:27:31 -0500

    all done with the virtual closet functionalities. orders section done.

--- Pages/profile.js
+++ Pages/profile.js
@@ -49,16 +49,13 @@ function cancleOrder(id) {
 function loadOrderItems() {
     const ordersItemsContainer = document.getElementById('orderItems');
     const orders = JSON.parse(localStorage.getItem('order')) || [];
-
     if (orders.length === 0) {
-        ordersItemsContainer.innerHTML = "<p class='no-orders'>You dont have any orders :(</p>";
+        ordersItemsContainer.innerHTML = "<p class='no-orders'>You don't have any orders :(</p>";
         return;
     }
-
     ordersItemsContainer.innerHTML = orders.map(item => {
         console.log(item.images)
         return `
-
             <div id='${item.id}' class="order">
                 <div class="order-img">
                     <img src="${item.images}" alt="${item.name}">
@@ -77,8 +74,78 @@ function loadOrderItems() {
             `;
     }).join("");
 }
+function loadClosetItems() {
+    const closetItemsContainer = document.getElementById('closetItems');
+    const closetItems = JSON.parse(localStorage.getItem('recommendations')) || [];
+    if (closetItems.length == 0) {
+        closetItemsContainer.innerHTML = "<p class='no-orders'>You don't have any outfits saved :(</p>"
+        return
+    }
+    // console.log(closetItems)
+    // for (let i = 0; i < closetItems.length; i++) {
+    //     const item = closetItems[i];
+    //     console.log(item)
+    // }
+
+    closetItems.map(itemarray => {
+        closetItemsContainer.innerHTML = itemarray.map(item => {            
+            console.log(item)
+            return `
+                <div id='${item.index}' class="recommendation">
+                    <div class="recommendation-img">
+                        <img src="${item.img}" alt="${item.name}">
+                    </div>
+                    <div class="order-details">
+                        <div id="recommendation-name">${item.name || "Unnamed Product"}</div>
+                    </div>
+                </div>
+                `;
+        }).join("");
+    })
+
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     updateCounter();
-    loadOrderItems()
+    loadOrderItems();
+    loadClosetItems();
 });
+//! 5222300179
+
+const ordersBtn = document.getElementById('orders');
+const closetBtn = document.getElementById('closet');
+const profileBtn = document.getElementById('profile');
+
+const ordersSection = document.getElementById('orders-section')
+const profileSection = document.getElementById('profile-section')
+const closetSection = document.getElementById('closet-section')
+
+ordersBtn.addEventListener('click', function(){
+    ordersBtn.classList.add('active')
+    profileBtn.classList.remove('active')
+    closetBtn.classList.remove('active')
+    
+    ordersSection.classList.remove('novisible')
+    profileSection.classList.add('novisible')
+    closetSection.classList.add('novisible')
+})
+
+profileBtn.addEventListener('click', function(){
+    ordersBtn.classList.remove('active')
+    profileBtn.classList.add('active')
+    closetBtn.classList.remove('active')
+
+    ordersSection.classList.add('novisible')
+    profileSection.classList.remove('novisible')
+    closetSection.classList.add('novisible')
+})
+
+closetBtn.addEventListener('click', function(){
+    ordersBtn.classList.remove('active')
+    profileBtn.classList.remove('active')
+    closetBtn.classList.add('active')
+
+    ordersSection.classList.add('novisible')
+    profileSection.classList.add('novisible')
+    closetSection.classList.remove('novisible')
+})
--- Test/testingmodule.js
+++ Test/testingmodule.js
@@ -1,3 +1,6 @@
-const order = `[{"id":"66f754f1f7dc272ad93260f4","name":"Pureboost 21 Shoes","quantity":1,"images":"https://assets.adidas.com/images/w_600,f_auto,q_auto/aa849ee2169741c8bbe4ad1300d53480_9366/Pureboost_21_Shoes_Purple_GY5110_42_detail.jpg","selling_price":91}]`
-const orderObject = JSON.parse(order)
-console.log(orderObject)
+var recommendation1 = [1,2,3,4,5]
+var recommendation2 = [5,6,7,8,9]
+var recommendations = []
+recommendations.push(recommendation1)
+recommendations.push(recommendation2)
+console.log(recommendations)
